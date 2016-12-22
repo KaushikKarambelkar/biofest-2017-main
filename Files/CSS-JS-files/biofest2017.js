57ad8803d3bfc7f6a7f341f75e69b86127c75fa7
KaushikKarambelkar <kaushik.iitmadras@gmail.com>
2016-12-22 20:56:26 +0530

    Events menu complete

--- Files/CSS-JS-files/biofest2017.js
+++ Files/CSS-JS-files/biofest2017.js
@@ -1,46 +1,223 @@
-$(document).ready(function(){	//Activate script only when entire document is ready
+$(document).ready(function(){ //Activate script only when entire document is ready
 
-   	 	$("#showmenu").click(function(){ //Shows main menu
+         $("#showmenu").click(function(){ //Shows main menu
 
-   	 	$("#showmenu").fadeOut(1000,function(){
-        $(".menu").fadeIn(1000);
+         $("#showmenu").fadeOut(1000,function(){
+         $(".menu").fadeIn(1000);
         });
 
-    	});
+      });
 
-   	 	//When symposium is clicked, shows symposium and hides everything else	
-   	 	$("#sym").click(function(){
-   	 		$("sym").fadeOut(1000);
-   	 		$("#eve").fadeOut(1000);
-   	 		$("#reg").fadeOut(1000);
-   	 		$("#spons").fadeOut(1000);
-   	 		$("#cont").fadeOut(1000);
-   	 		$("#team").fadeOut(1000);
-   	 		$(".symmenu").fadeIn(1000);
+         //When symposium is clicked, shows symposium and hides everything else  
+         $("#sym").click(function(){
+            $("#sym").fadeOut(1000);
+            $("#eve").fadeOut(1000);
+            $("#reg").fadeOut(1000);
+            $("#spons").fadeOut(1000);
+            $("#cont").fadeOut(1000);
+            $("#team").fadeOut(1000);
+            $(".symmenu").fadeIn(1000);
 
-   	 		/* $("#sym").css({
-   	 			"display": "block",
-   	 			"width": "100%"
-   	 		}, 1000); */
+            /* $("#sym").css({
+               "display": "block",
+               "width": "100%"
+            }, 1000); */
 
-   	 	});
+         });
 
-   	 	//When events is clicked, shows events and hides everything else
-   	 	$("#eve").click(function(){
-   	 		$("#eve").fadeOut(1000);
-   	 		$("#sym").fadeOut(1000);
-   	 		$("#reg").fadeOut(1000);
-   	 		$("#spons").fadeOut(1000);
-   	 		$("#cont").fadeOut(1000);
-   	 		$("#team").fadeOut(1000);
-   	 		$(".evemenu").fadeIn(2000);
+         //When events is clicked, shows events and hides everything else
+         $("#eve").click(function(){
+            $("#sym").hide(1000);
+            $("#reg").hide(1000);
+            $("#spons").hide(1000);
+            $("#cont").hide(1000);
+            $("#team").hide(1000, function(){
+               $("#eve").animate({
+                  "left": "0",
+                  "width": "100%"
+               }, 1000, function(){
+                  $("#eve").fadeOut(1000);
+                  $(".evemenu").fadeIn(1000);
+               });
+            });
 
 
-   	 		/*$("#eve").css({
-   	 			"left": "0",
-   	 			"display": "block",
-   	 			"width": "100%"
-   	 		}, 1000);*/
+            /*$("#eve").css({
+               "left": "0",
+               "display": "block",
+               "width": "100%"
+            }, 1000);*/
 
-   	 	});
-	});
+         });
+
+         //Shows Forensics page when clicked   
+
+         $("#forensics").click(function(){
+
+            $("#streax").hide(1000);
+            $("#paperposter").hide(1000);
+            $("#lectures").hide(1000);
+            $("#workshops").hide(1000);
+            $("#idp").hide(1000);
+            $("#quiz").hide(1000);
+            $("#biobiz").hide(1000);
+            $("#gbc").hide(1000, function(){
+                  $(".evemenu").animate({
+                     "width": "100%"
+                  },1000);
+            });
+         });
+
+         //Shows streax page when clicked
+
+         $("#streax").click(function(){
+
+            $("#forensics").hide(1000);
+            $("#paperposter").hide(1000);
+            $("#lectures").hide(1000);
+            $("#workshops").hide(1000);
+            $("#idp").hide(1000);
+            $("#quiz").hide(1000);
+            $("#biobiz").hide(1000);
+            $("#gbc").hide(1000, function(){
+                  $(".evemenu").animate({
+                     "width": "100%",
+                     "left": "0"
+                  },1000);
+            });
+         });
+
+         //Shows paper and poster page when clicked
+
+         $("#paperposter").click(function(){
+
+            $("#forensics").hide(1000);
+            $("#streax").hide(1000);
+            $("#lectures").hide(1000);
+            $("#workshops").hide(1000);
+            $("#idp").hide(1000);
+            $("#quiz").hide(1000);
+            $("#biobiz").hide(1000);
+            $("#gbc").hide(1000, function(){
+                  $(".evemenu").animate({
+                     "width": "100%",
+                     "left": "0"
+                  },1000);
+            });
+         });
+
+         //Shows Forensics page when clicked
+
+         $("#lectures").click(function(){
+
+            $("#forensics").hide(1000);
+            $("#paperposter").hide(1000);
+            $("#streax").hide(1000);
+            $("#workshops").hide(1000);
+            $("#idp").hide(1000);
+            $("#quiz").hide(1000);
+            $("#biobiz").hide(1000);
+            $("#gbc").hide(1000, function(){
+                  $(".evemenu").animate({
+                     "width": "100%",
+                     "left": "0"
+                  },1000);
+            });
+         });
+
+         //Shows workshops page when clicked
+
+         $("#workshops").click(function(){
+
+            $("#forensics").hide(1000);
+            $("#paperposter").hide(1000);
+            $("#lectures").hide(1000);
+            $("#streax").hide(1000);
+            $("#idp").hide(1000);
+            $("#quiz").hide(1000);
+            $("#biobiz").hide(1000);
+            $("#gbc").hide(1000, function(){
+                  $(".evemenu").animate({
+                     "width": "100%",
+                     "left": "0"
+                  },1000);
+            });
+         });
+
+         //Shows IDP page when clicked
+
+         $("#idp").click(function(){
+
+            $("#forensics").hide(1000);
+            $("#paperposter").hide(1000);
+            $("#lectures").hide(1000);
+            $("#workshops").hide(1000);
+            $("#streax").hide(1000);
+            $("#quiz").hide(1000);
+            $("#biobiz").hide(1000);
+            $("#gbc").hide(1000, function(){
+                  $(".evemenu").animate({
+                     "width": "100%",
+                     "left": "0"
+                  },1000);
+            });
+         });
+
+         //Shows Quiz page when clicked
+
+         $("#quiz").click(function(){
+
+            $("#forensics").hide(1000);
+            $("#paperposter").hide(1000);
+            $("#lectures").hide(1000);
+            $("#workshops").hide(1000);
+            $("#idp").hide(1000);
+            $("#streax").hide(1000);
+            $("#biobiz").hide(1000);
+            $("#gbc").hide(1000, function(){
+                  $(".evemenu").animate({
+                     "width": "100%",
+                     "left": "0"
+                  },1000);
+            });
+         });
+
+         //Shows BioBiz page when clicked
+
+         $("#biobiz").click(function(){
+
+            $("#forensics").hide(1000);
+            $("#paperposter").hide(1000);
+            $("#lectures").hide(1000);
+            $("#workshops").hide(1000);
+            $("#idp").hide(1000);
+            $("#quiz").hide(1000);
+            $("#streax").hide(1000);
+            $("#gbc").hide(1000, function(){
+                  $(".evemenu").animate({
+                     "width": "100%",
+                     "left": "0"
+                  },1000);
+            });
+         });
+
+         //Shows Global Biotech Conference page when clicked
+
+         $("#gbc").click(function(){
+
+            $("#forensics").hide(1000);
+            $("#paperposter").hide(1000);
+            $("#lectures").hide(1000);
+            $("#workshops").hide(1000);
+            $("#idp").hide(1000);
+            $("#quiz").hide(1000);
+            $("#biobiz").hide(1000);
+            $("#streax").hide(1000, function(){
+                  $(".evemenu").animate({
+                     "width": "100%",
+                     "left": "0"
+                  },1000);
+            });
+         });
+
+});
