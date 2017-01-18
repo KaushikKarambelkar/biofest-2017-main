18ce76f979b7ca9dc919f183f440c771e0420c0d
KaushikKarambelkar <kaushik.iitmadras@gmail.com>
2017-01-18 16:19:26 +0530

    Added icon to redirect back to events menu from events pages

--- Files/CSS-JS-files/biofest2017.js
+++ Files/CSS-JS-files/biofest2017.js
@@ -72,8 +72,90 @@ $(document).ready(function(){ //Activate script only when entire document is rea
       });
 
 
-         //Redirect back to events menu from events pages
-         $(".backtoevemenufromforensicsfromforensics").click(function(){
+         //Redirect back to events menu from forensics page
+
+         $(".backtoevemenu").click(function(){
+
+            $("#gbcdescription").fadeOut(200);
+            $("#gbcregistration").fadeOut(200);
+            $("#gbcrules").fadeOut(200);
+            $("#gbccontact").fadeOut(200);
+            $("#gbccall").fadeOut(200);
+            $("#gbcemail").fadeOut(200);
+            $("#gbcphn").fadeOut(200);
+            $("#gbcarchive").fadeOut(200);
+            $("#gbcemailid").fadeOut(200);
+
+            $("#biobizdescription").fadeOut(200);
+            $("#biobizregistration").fadeOut(200);
+            $("#biobizrules").fadeOut(200);
+            $("#biobizcontact").fadeOut(200);
+            $("#biobizcall").fadeOut(200);
+            $("#biobizemail").fadeOut(200);
+            $("#biobizphn").fadeOut(200);
+            $("#biobizarchive").fadeOut(200);
+            $("#biobizemailid").fadeOut(200);
+
+            $("#quizdescription").fadeOut(200);
+            $("#quizregistration").fadeOut(200);
+            $("#quizrules").fadeOut(200);
+            $("#quizcontact").fadeOut(200);
+            $("#quizcall").fadeOut(200);
+            $("#quizemail").fadeOut(200);
+            $("#quizphn").fadeOut(200);
+            $("#quizarchive").fadeOut(200);
+            $("#quizemailid").fadeOut(200);
+
+            $("#idpdescription").fadeOut(200);
+            $("#idpregistration").fadeOut(200);
+            $("#idprules").fadeOut(200);
+            $("#idpcontact").fadeOut(200);
+            $("#idpcall").fadeOut(200);
+            $("#idpemail").fadeOut(200);
+            $("#idpphn").fadeOut(200);
+            $("#idparchive").fadeOut(200);
+            $("#idpemailid").fadeOut(200);
+
+            $("#workshopsdescription").fadeOut(200);
+            $("#workshopsregistration").fadeOut(200);
+            $("#workshopsrules").fadeOut(200);
+            $("#workshopscontact").fadeOut(200);
+            $("#workshopscall").fadeOut(200);
+            $("#workshopsemail").fadeOut(200);
+            $("#workshopsphn").fadeOut(200);
+            $("#workshopsarchive").fadeOut(200);
+            $("#workshopsemailid").fadeOut(200);
+
+            $("#lecturesdescription").fadeOut(200);
+            $("#lecturesregistration").fadeOut(200);
+            $("#lecturesrules").fadeOut(200);
+            $("#lecturescontact").fadeOut(200);
+            $("#lecturescall").fadeOut(200);
+            $("#lecturesemail").fadeOut(200);
+            $("#lecturesphn").fadeOut(200);
+            $("#lecturesarchive").fadeOut(200);
+            $("#lecturesemailid").fadeOut(200);
+
+            $("#paperandposterdescription").fadeOut(200);
+            $("#paperandposterregistration").fadeOut(200);
+            $("#paperandposterrules").fadeOut(200);
+            $("#paperandpostercontact").fadeOut(200);
+            $("#paperandpostercall").fadeOut(200);
+            $("#paperandposteremail").fadeOut(200);
+            $("#paperandposterphn").fadeOut(200);
+            $("#paperandposterarchive").fadeOut(200);
+            $("#paperandposteremailid").fadeOut(200);
+
+            $("#streaxdescription").fadeOut(200);
+            $("#streaxregistration").fadeOut(200);
+            $("#streaxrules").fadeOut(200);
+            $("#streaxcontact").fadeOut(200);
+            $("#streaxcall").fadeOut(200);
+            $("#streaxemail").fadeOut(200);
+            $("#streaxphn").fadeOut(200);
+            $("#streaxarchive").fadeOut(200);
+            $("#streaxemailid").fadeOut(200);
+
             $("#forensicsdescription").fadeOut(200);
             $("#forensicsregistration").fadeOut(200);
             $("#forensicsrules").fadeOut(200);
@@ -93,25 +175,83 @@ $(document).ready(function(){ //Activate script only when entire document is rea
 
                $(".firstletterinevents").animate({
                      "font-size": "2.5vw"
-               }, 500, function(){
-                  $(".evemenu").show(1000);
-
-                  $("#forensics").animate({
-                     "left": "0",
-                     "top": "0",
-                     "width": "11.11%",
-                     "padding-left": "1%",
-                     "text-align": "center"
-                  });
-
-                  $("#streax").animate({
-                     "left": "0",
-                     "top": "0",
-                     "width": "11.11%",
-                     "padding-left": "12.11%",
-                     "text-align": "center"
-                  });
                });
+
+               $(".evemenu").show(1000);
+
+               $("#forensics").animate({
+                  "left": "0",
+                  "top": "0",
+                  "width": "11.11%",
+                  "padding-left": "1%",
+                  "text-align": "center"
+               });
+
+               $("#streax").animate({
+                  "left": "11.11%",
+                  "top": "0",
+                  "width": "11.11%",
+                  "padding-left": "2%",
+                  "text-align": "center"
+               });
+
+               $("#paperandposter").animate({
+                  "left": "22.22%",
+                  "top": "0",
+                  "width": "11.11%",
+                  "padding-left": "2.4%",
+                  "text-align": "center"
+               });
+
+               $("#lectures").animate({
+                  "left": "33.33%",
+                  "top": "0",
+                  "width": "11.11%",
+                  "padding-left": "1.3%",
+                  "text-align": "center"
+               });
+
+               $("#workshops").animate({
+                  "left": "44.44%",
+                  "top": "0",
+                  "width": "11.11%",
+                  "padding-left": "0.1%",
+                  "text-align": "center"
+               });
+
+               $("#idp").animate({
+                  "left": "55.55%",
+                  "top": "0",
+                  "width": "11.11%",
+                  "padding-left": "0.8%",
+                  "text-align": "center"
+               });
+
+               $("#quiz").animate({
+                  "left": "66.66%",
+                  "top": "0",
+                  "width": "11.11%",
+                  "padding-left": "3%",
+                  "text-align": "center"
+               });
+
+               $("#biobiz").animate({
+                  "left": "77.77%",
+                  "top": "0",
+                  "width": "11.11%",
+                  "padding-left": "2.3%",
+                  "text-align": "center"
+               });
+
+               $("#gbc").animate({
+                  "left": "88.88%",
+                  "top": "0",
+                  "width": "11.11%",
+                  "padding-left": "0.5%",
+                  "text-align": "center"
+               });
+
+               $(".backtoevemenufromforensics").fadeOut();
             }); 
          });
 
@@ -136,7 +276,7 @@ $(document).ready(function(){ //Activate script only when entire document is rea
                      "padding-top": "4%",
                      "padding-right": "80%"
                   }, 500, function(){
-                        $(".backtoevemenufromforensics").fadeIn(200);
+                        $(".backtoevemenu").fadeIn(200);
                         $("#forensicsdescription").fadeIn(200);
                         $("#forensicsregistration").fadeIn(200);
                         $("#forensicsrules").fadeIn(200);
@@ -153,7 +293,6 @@ $(document).ready(function(){ //Activate script only when entire document is rea
          //Shows streax page when clicked
 
          $("#streax").click(function(){
-
             $("#forensics").hide(1000);
             $("#paperandposter").hide(1000);
             $("#lectures").hide(1000);
@@ -173,6 +312,7 @@ $(document).ready(function(){ //Activate script only when entire document is rea
                      "padding-top": "4%",
                      "padding-right": "80%"
                   }, 500, function(){
+                     $(".backtoevemenu").fadeIn(200);
                      $("#streaxdescription").fadeIn(200);
                      $("#streaxregistration").fadeIn(200);
                      $("#streaxrules").fadeIn(200);
@@ -209,6 +349,7 @@ $(document).ready(function(){ //Activate script only when entire document is rea
                      "padding-top": "4%",
                      "padding-right": "80%"
                   }, 500, function(){
+                     $(".backtoevemenu").fadeIn(200);
                      $("#paperandposterdescription").fadeIn(200);
                      $("#paperandposterregistration").fadeIn(200);
                      $("#paperandposterrules").fadeIn(200);
@@ -245,6 +386,7 @@ $(document).ready(function(){ //Activate script only when entire document is rea
                      "padding-top": "4%",
                      "padding-right": "80%"
                   }, 500, function(){
+                     $(".backtoevemenu").fadeIn(200);
                      $("#lecturesdescription").fadeIn(200);
                      $("#lecturesregistration").fadeIn(200);
                      $("#lecturesrules").fadeIn(200);
@@ -281,6 +423,7 @@ $(document).ready(function(){ //Activate script only when entire document is rea
                      "padding-top": "4%",
                      "padding-right": "80%"
                   }, 500, function(){
+                     $(".backtoevemenu").fadeIn(200);
                      $("#workshopsdescription").fadeIn(200);
                      $("#workshopsregistration").fadeIn(200);
                      $("#workshopsrules").fadeIn(200);
@@ -317,6 +460,7 @@ $(document).ready(function(){ //Activate script only when entire document is rea
                      "padding-top": "4%",
                      "padding-right": "80%"
                   }, 500, function(){
+                     $(".backtoevemenu").fadeIn(200);
                      $("#idpdescription").fadeIn(200);
                      $("#idpregistration").fadeIn(200);
                      $("#idprules").fadeIn(200);
@@ -353,6 +497,7 @@ $(document).ready(function(){ //Activate script only when entire document is rea
                      "padding-top": "4%",
                      "padding-right": "80%"
                   }, 500, function(){
+                     $(".backtoevemenu").fadeIn(200);
                      $("#quizdescription").fadeIn(200);
                      $("#quizregistration").fadeIn(200);
                      $("#quizrules").fadeIn(200);
@@ -389,6 +534,7 @@ $(document).ready(function(){ //Activate script only when entire document is rea
                      "padding-top": "4%",
                      "padding-right": "80%"
                   }, 500, function(){
+                     $(".backtoevemenu").fadeIn(200);
                      $("#biobizdescription").fadeIn(200);
                      $("#biobizregistration").fadeIn(200);
                      $("#biobizrules").fadeIn(200);
@@ -429,6 +575,7 @@ $(document).ready(function(){ //Activate script only when entire document is rea
                      "padding-top": "4%",
                      "padding-right": "80%"
                   }, 500, function(){
+                     $(".backtoevemenu").fadeIn(200);
                      $("#gbcdescription").fadeIn(200);
                      $("#gbcregistration").fadeIn(200);
                      $("#gbcrules").fadeIn(200);
